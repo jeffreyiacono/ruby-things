e906801935569942e86f9b875f185097ede21403
Jeff Iacono <iacono@squareup.com>
2012-10-05 18:45:08 -0700

    [TRIANGLE] parens are optional

--- triangle-checker.rb
+++ triangle-checker.rb
@@ -1,4 +1,4 @@
-def triangle?(a, b, c)
+def triangle? a, b, c
   a + b > c
 end
 
@@ -8,7 +8,7 @@ triangles = 0
 (1..N).each do
   breaks = [rand, rand].sort
   lengths = [breaks[0], breaks[1] - breaks[0], 1 - breaks[1]].sort
-  triangles += 1 if triangle?(lengths[0], lengths[1], lengths[2])
+  triangles += 1 if triangle? lengths[0], lengths[1], lengths[2]
 end
 
 puts triangles.to_f / N
