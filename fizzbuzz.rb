72d4b4fe559c4b03d0d050daffc95385c07a2bde
Jeff Iacono <iacono@squareup.com>
2012-10-05 18:11:36 -0700

    [FIZZBUZZ] Using a Moderator class
    Notice the use of (1..100).each, this is another method of iterating over a set
    of objects that you should prefer over for-loops
    
    This is definitely overkill for this problem, but should serve as an example of
    what you can do with classes
    
    Also note that this is **not** the best use-case for a class because we really
    don't encapsulate internal state in the Moderator. A module might be more
    appropriate or a generic function.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,30 @@
-1.upto(100) do |i|
-  if i % 15 == 0
-    puts "FizzBuzz"
-  elsif i % 3 == 0 
-    puts "Fizz"
-  elsif i % 5 == 0
-    puts "Buzz"
-  else
-    puts i
+# moderator class so we can encapsulate everything and if we need to change the
+# moderation (#moderate) algo, we can easily do that without messing with the rest
+# of the program
+class Moderator
+  # moderate algo:
+  # if number is divisible by 3 and 5 (lcm = 15), return "FizzBuzz"
+  # else if number is divisible by only 3, return "Fizz"
+  # else if number is divisible by only 5, return "Buzz"
+  # otherwise just return the number
+  def moderate(i)
+    if i % 15
+      return "FizzBuzz"
+    elsif i % 3 == 0
+      return "Fizz"
+    elsif i % 5 == 0
+      return "Buzz"
+    else
+      return i
+    end
   end
+end
+
+# can I get a moderator up in here?
+moderator = Moderator.new
+
+# moderate the range 1 - 100, inclusive
+# print the moderated result
+(1..100).each do |i|
+  puts moderator.moderate(i)
 end
