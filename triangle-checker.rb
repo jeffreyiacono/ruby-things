237783e8bcbd851f16d82a07e01b612b8cab649b
Jeff Iacono <iacono@squareup.com>
2012-10-05 18:42:48 -0700

    [TRIANGLE] use more explicit variable name

--- triangle-checker.rb
+++ triangle-checker.rb
@@ -3,11 +3,11 @@ def triangle?(a, b, c)
 end
 
 N = 100000
-p = 0
+triangles = 0
 for i in 1..N
   breaks = [rand, rand].sort
   lengths = [breaks[0], breaks[1] - breaks[0], 1 - breaks[1]].sort
-  p += 1 if triangle?(lengths[0], lengths[1], lengths[2])
+  triangles += 1 if triangle?(lengths[0], lengths[1], lengths[2])
 end
 
-puts p.to_f / N
+puts triangles.to_f / N
