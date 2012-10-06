644fa32866a954cd208ef0ea2ae12e0246e6f135
Jeff Iacono <iacono@squareup.com>
2012-10-05 18:36:40 -0700

    [TRIANGLE] for methods that return true / false, use [function_name]? syntax (ruby)

--- triangle-checker.rb
+++ triangle-checker.rb
@@ -1,4 +1,4 @@
-def check_triangle(a, b, c)
+def triangle?(a, b, c)
   a + b > c
 end
 
@@ -7,7 +7,7 @@ p = 0
 for i in 1..N
   breaks = [rand, rand].sort
   lengths = [breaks[0], breaks[1] - breaks[0], 1 - breaks[1]].sort
-  p += 1 if check_triangle(lengths[0], lengths[1], lengths[2])
+  p += 1 if triangle?(lengths[0], lengths[1], lengths[2])
 end
 
 puts p.to_f / N
